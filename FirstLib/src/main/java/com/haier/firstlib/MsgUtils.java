3205efd7a332bf2753880da6e6d0fa5b618002d1
ziliangzxc <liziliangzxc@163.com>
2022-07-22 14:46:26 +0800

    第二次上传测试

--- FirstLib/src/main/java/com/haier/firstlib/MsgUtils.java
+++ FirstLib/src/main/java/com/haier/firstlib/MsgUtils.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class MsgUtils {
-    public static  void show(Context context,String content){
+    public static void show(Context context,String content){
+        Toast.makeText(context,content,Toast.LENGTH_SHORT).show();
+    }
+
+    public static void show2(Context context,String content){
         Toast.makeText(context,content,Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/haier/message/MainActivity.java
+++ app/src/main/java/com/haier/message/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.haier.firstlib.MsgUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MsgUtils.show(this,"git库测试");
     }
 }
